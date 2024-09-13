3e888e51747f083090ea31371d14281df5829db0
Cade Mitchell <cademitchell7@outlook.com>
2024-09-13 15:38:18 -0400

    Started Groundwork for fruit picker app.

---
+++ Fruit Picker.py
@@ -0,0 +1,24 @@
+import json
+
+def import_json_file(file_name: str):
+    pass
+
+def export_json_file(dictionary: dict):
+    pass
+
+
+
+
+def main():
+    #File Select
+    file = None
+    
+    #Menu
+    
+    #Do selected action
+    
+    
+
+
+if __name__ == "__main__":
+    main()
