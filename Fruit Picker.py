57ff32c9922f59c7577e82eee740eab92fcbb9ec
Cade Mitchell <cademitchell7@outlook.com>
2024-09-16 16:29:42 -0400

    Added feateres

--- Fruit Picker.py
+++ Fruit Picker.py
@@ -67,11 +67,58 @@ def main():
             new_fruit = Fruit(fruit_name, fruit_type, fruit_desc, fruit_weight, abilities, fruit_available)
             file["fruit"].append(new_fruit.export())
                 
-            
         elif choice == "Edit":
-            pass
+            while True:
+                option = [("Exit Menu", "Exit")]
+                option.extend([(fruit["fruit_name"], fruit) for fruit in file["fruit"]])
+                edit_fruit_choice = menu_generator(option, "Select which fruit you would like to edit, or 'Exit Menu' to retrun to main screen.")
+                         
+                if edit_fruit_choice == "Exit":
+                    break
+                
+                fruit_obj = Fruit(edit_fruit_choice["fruit_name"], edit_fruit_choice["type"], edit_fruit_choice["weight"], 
+                                    edit_fruit_choice["fruit_desc"], edit_fruit_choice["abilities"] ,edit_fruit_choice["available"])
+                while True:
+                    edit_option = [("Exit Menu", "Exit"), ("Edit Fruit Name", "Name"), ("Edit Fruit Type", "Type"), 
+                                    ("Edit Fruit Description", "Desc"), ("Edit Fruit Weight", "Weight"), ("Toggle Fruit Availability", "Available"), 
+                                    ("Add Fruit Ability", "Add"), ("Remove Fruit Ability", "Remove")]
+                    edit_choice = menu_generator(edit_option, fruit_obj)
+                    if edit_choice == "Exit":
+                        index = file["fruit"].index(edit_fruit_choice)
+                        file["fruit"][index] = fruit_obj.export()
+                        break
+                    elif edit_choice == "Name":
+                        fruit_obj.edit_name(input("Enter New Name: "))
+                    elif edit_choice == "Type":
+                        fruit_obj.edit_fruit_type(input("Enter New Fruit Type: "))
+                    elif edit_choice == "Desc":
+                        fruit_obj.edit_desc(input("Enter New Fruit Description:\n"))
+                    elif edit_choice == "Weight":
+                        fruit_obj.edit_weight(input("Enter New Weight Chance: "))
+                    elif edit_choice == "Available":
+                        fruit_obj.toggle_avaiable()
+                    elif edit_choice == "Add":
+                        ability_name = input("Please type new ability name: ")
+                        ability_desc = input("Please type new ability description:\n")
+                        fruit_obj.add_ability(ability_name, ability_desc)
+                    elif edit_choice == "Remove":
+                        abilities_options = [("Cancel", "Exit")]
+                        abilities_options.extend([(ability["name"], ability["name"]) for ability in fruit_obj.abilities])
+                        ability_remove_choice = menu_generator(abilities_options, "Select which fruit you would like to edit, or 'Cancel' to retrun to previous menu.")
+                        if ability_remove_choice != "Exit":
+                            fruit_obj.del_ability(ability_remove_choice)    
+        
         elif choice == "Delete":
-            pass
+            while True:
+                option = []
+                option.append(("Exit Menu", "Exit"))
+                option.extend([(fruit["fruit_name"], fruit) for fruit in file["fruit"]])
+                del_choice = menu_generator(option, "Select which fruit you would like to delete, or 'Exit Menu' to retrun to main screen.")       
+                if del_choice == "Exit":
+                    break          
+                index = file["fruit"].index(del_choice)
+                file["fruit"].pop(index)
+            
 
 
 if __name__ == "__main__":
